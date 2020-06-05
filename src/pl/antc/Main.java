e9e53637c06c10534f129eb2daf9ad25fcd19ebe
ACharchula <antoni.charchula@gmail.com>
2020-06-05 20:31:01 +0200

    shuffled adult dataset

--- src/pl/antc/Main.java
+++ src/pl/antc/Main.java
@@ -6,11 +6,11 @@ import pl.antc.csv.PrepareData;
 public class Main {
 
     public static void main(String[] args) throws Exception {
-        //PrepareData.prepareAdultData();
+//        PrepareData.prepareAdultData();
 //        PrepareData.prepareCarData();
         //PrepareData.prepareNurseryData();
-        //AlgorithmRunner.runCN2("data/adult/training.data", "data/adult/test.data", 2, 0.10);
+        //AlgorithmRunner.runCN2("data/adult/training.data", "data/adult/test.data", 2, 0.4);
         //AlgorithmRunner.runCN2("data/cars/training.data", "data/cars/test.data", 2, 0.3);
-        AlgorithmRunner.runCN2("data/nursery/training.data", "data/nursery/test.data", 2, 0.2);
+        //AlgorithmRunner.runCN2("data/nursery/training.data", "data/nursery/test.data", 2, 0.2);
     }
 }
